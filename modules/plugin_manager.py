c54e8cdc25cb91345b45fb32e15960a955da23d1
maglore9900 <78699112+maglore9900@users.noreply.github.com>
2025-04-17 08:42:44 -0400

    auto load true for enabled plugins

--- modules/plugin_manager.py
+++ modules/plugin_manager.py
@@ -15,7 +15,7 @@ class PluginManager:
         self.active_plugin = None  # Currently active plugin
         self.settings_file = settings_file
         self.settings = {
-            'auto_load_plugins': False,  # Default to not auto-loading plugins
+            'auto_load_plugins': True,  # Default to not auto-loading plugins
             'enabled_plugins': []  # List of enabled plugin names
         }
         
